c74dfc6e5734fb6aed5dd6fa40e984b1aabfdc30
Dylan Kauling <gunsmithy@gmail.com>
2020-04-10 17:10:13 -0400

    Updated Data Centre list. Added command-line support.

--- README.md
+++ README.md
@@ -10,10 +10,20 @@ You will be prompted for an account UUID which can be found in the URL after sea
 
 Right now there is no local storage of account UUIDs or names, so the easiest thing to do is have it automatically detect and change the server for all accounts on your PC.
 
+## Command-Line usage
+Command-line use is now supported! Simply include one or both of the below parameters to skip the user prompt.
+
+* --account ACCOUNT - The account UUID for which you want to change servers. Assumed to be all accounts found locally if not provided.
+* --server SERVER - The server/data-centre you wish to use. Assumed to be default/ping-based if not provided.
+
+You can use this with a Stream Deck "Open" action for example by pointing to `siege_server_changer.exe` and including the parameters after it in the text box.  
+
+Example of what you would include in the App/File box of the Open action:  
+`C:\Users\User\Downloads\siege_server_changer-v0.2\siege_server_changer.exe --account="3cb45a4a-9208-48ac-8690-27dcbf1b6604" --server="eastus"`
+
 ## Future Releases
 Future releases will likely include:
 * Local storage of account UUIDs/querying R6Tab for names.
-* Command-line/parameter support for one-button changing/easy runs with shortcuts/Stream Deck
 * A GUI instead of command prompt when not opting to use parameters/shortcuts
 
 ## Enjoy!
--- siege_server_changer.py
+++ siege_server_changer.py
@@ -2,22 +2,25 @@ from pathlib import Path
 from uuid import UUID
 import configparser
 import sys
+import argparse
 
 SECTION_NAME = 'ONLINE'
 OPTION_NAME = 'DataCenterHint'
 DATA_CENTRES = {
-    'default': 'ping based',
-    'eus': 'us east',
-    'cus': 'us central',
-    'scus': 'us south central',
-    'wus': 'us west',
-    'sbr': 'brazil south',
-    'neu': 'europe north',
-    'weu': 'europe west',
-    'eas': 'asia east',
-    'seas': 'asia south east',
-    'eau': 'australia east',
-    'wja': 'japan west',
+    'default': 'Default (Ping-based)',
+    'eastus': 'US (East)',
+    'centralus': 'US (Central)',
+    'southcentralus': 'US (South Central)',
+    'westus': 'US (West)',
+    'brazilsouth': 'Brazil (South)',
+    'northeurope': 'Europe (North)',
+    'westeurope': 'Europe (West)',
+    'southafricanorth': 'South Africa (North)',
+    'eastasia': 'Asia (East)',
+    'southeastasia': 'Asia (South East)',
+    'australiaeast': 'Australia (East)',
+    'australiasoutheast': 'Australia (South East)',
+    'japanwest': 'Japan (West)',
 }
 
 
@@ -56,14 +59,32 @@ def set_config(account_uuid, server):
     print("Server for", account_uuid, "has been set to", server)
 
 
-def main():
-    print("Input the account for which you would like to change the data centre.")
-    print("Provide the account ID which can be found in the URL of its R6Tab profile.")
-    print("Alternatively, input nothing to default to changing the server for all accounts found on this machine.")
-    account_input = input("Account UUID: ").strip()
+def args_config():
+    parser = argparse.ArgumentParser(description='Change R6:Siege servers/data-centre in your config file.')
+    parser.add_argument('--account', dest='account', help='The account UUID for which you want to change servers. '
+                                                          'Assumed to be all accounts found locally if not provided.')
+    parser.add_argument('--server', dest='server', help='The server/data-centre you wish to use. Assumed to be '
+                                                        'default/ping-based if not provided.')
+    args = parser.parse_args()
 
+    if args.account is not None:
+        print('Using --account command-line argument...')
+        account = args.account
+    else:
+        account = None
+
+    if args.server is not None:
+        print('Using --server command-line argument...')
+        server = args.server
+    else:
+        server = None
+
+    return account, server
+
+
+def check_account(account_input):
     accounts = []
-    if account_input != "":
+    if account_input and account_input != "":
         try:
             UUID(account_input)
         except ValueError:
@@ -73,23 +94,53 @@ def main():
         read_config(account_input)
         accounts.append(account_input)
     else:
+        print("No account provided. Assuming all accounts...")
         accounts = find_accounts()
+
+    return accounts
+
+
+def check_server(server_input):
+    if server_input == "" or server_input is None:
+        print("No server provided. Assuming default/ping-based...")
+        server_output = "default"
+    else:
+        if not DATA_CENTRES.get(server_input):
+            print("Invalid data centre provided. Please confirm your entry and try again.")
+            sys.exit(2)
+        else:
+            server_output = server_input
+
+    return server_output
+
+
+def prompt():
+    print("Input the account for which you would like to change the data centre.")
+    print("Provide the account ID which can be found in the URL of its R6Tab profile.")
+    print("Alternatively, input nothing to default to changing the server for all accounts found on this machine.")
+    account_input = input("Account UUID: ").strip()
 
     print("\nInput the server/data centre you would like to use.")
     print("This can be one of the following:", ", ".join(DATA_CENTRES))
     print("Alternatively, input nothing to default to setting the server back to default (Ping-based).")
     server_input = input("Server: ").strip()
 
-    if server_input == "":
-        server_input = "default"
-    else:
-        if not DATA_CENTRES.get(server_input):
-            print("Invalid data centre provided. Please confirm your entry and try again.")
-            sys.exit(2)
+    return account_input, server_input
 
+
+def main(accounts, server):
     for account in accounts:
-        set_config(account, server_input)
+        set_config(account, server)
 
 
 if __name__ == "__main__":
-    main()
+    config_account, config_server = args_config()
+    if config_account or config_server:
+        input_accounts = check_account(config_account)
+        input_server = check_server(config_server)
+    else:
+        prompted_account, prompted_server = prompt()
+        input_accounts = check_account(prompted_account)
+        input_server = check_server(prompted_server)
+
+    main(input_accounts, input_server)
